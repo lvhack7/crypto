323dab8216e2b42e0dbd7bbd9c7bb1a153fa046f
Khaknazar Abilda <hacki117@Khaknazars-MacBook-Air.local>
2023-11-18 14:11:05 +0600

    fix

--- script.js
+++ script.js
@@ -43,6 +43,58 @@ document.getElementById("contact-mobile").addEventListener('click', function() {
     }
 })
 
+document.getElementById("plus-1").addEventListener('click', function() {
+    const icon = document.getElementById("plus-1")
+    const content = document.getElementById("content-1")
+
+    if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden")
+        icon.src = "./imgs/close.svg"
+    } else {
+        content.classList.add("hidden")
+        icon.src = "./imgs/plus.svg"
+    }
+})
+
+document.getElementById("plus-2").addEventListener('click', function() {
+    const icon = document.getElementById("plus-2")
+    const content = document.getElementById("content-2")
+
+    if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden")
+        icon.src = "./imgs/close.svg"
+    } else {
+        content.classList.add("hidden")
+        icon.src = "./imgs/plus.svg"
+    }
+})
+
+document.getElementById("plus-3").addEventListener('click', function() {
+    const icon = document.getElementById("plus-3")
+    const content = document.getElementById("content-3")
+
+    if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden")
+        icon.src = "./imgs/close.svg"
+    } else {
+        content.classList.add("hidden")
+        icon.src = "./imgs/plus.svg"
+    }
+})
+
+document.getElementById("plus-4").addEventListener('click', function() {
+    const icon = document.getElementById("plus-4")
+    const content = document.getElementById("content-4")
+
+    if (content.classList.contains("hidden")) {
+        content.classList.remove("hidden")
+        icon.src = "./imgs/close.svg"
+    } else {
+        content.classList.add("hidden")
+        icon.src = "./imgs/plus.svg"
+    }
+})
+
 function toggleMenu() {
     var mobileMenu = document.getElementById('mobile-menu');
     var body = document.body
